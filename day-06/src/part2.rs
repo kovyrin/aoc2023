a62f30dda53c9e727268369dfc61140abab49740
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-12-06 14:40:46 -0500

    Day 6 part 2

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,10 +1,39 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let lines = input.lines().collect::<Vec<_>>();
+    let times = lines[0]
+        .split_whitespace()
+        .filter(|s| s != &"Time:")
+        .collect::<Vec<_>>()
+        .join("")
+        .parse::<u64>()
+        .unwrap();
+
+    let distances = lines[1]
+        .split_whitespace()
+        .filter(|s| s != &"Distance:")
+        .collect::<Vec<_>>()
+        .join("")
+        .parse::<u64>()
+        .unwrap();
+
+    // Merge times and distances into a vector of tuples
+    let race = (times, distances);
+    let (race_time, best_distance) = race;
+    let mut result = 0;
+    println!("Race: time={}, distance={}", race_time, best_distance);
+
+    for time in 1..race_time - 1 {
+        let speed = time;
+        let distance = (race_time - time) * speed;
+        if distance > best_distance {
+            result += 1;
+        }
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -13,9 +42,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Time:      7  15   30
+                     Distance:  9  40  200";
+        assert_eq!("71503", process(input)?);
         Ok(())
     }
 }
