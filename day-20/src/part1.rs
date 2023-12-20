84988ca010fec69a6979f040435d6b329d0653fa
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-12-20 11:49:01 -0500

    Day 20 part 1 tests pass

--- day-20/src/part1.rs
+++ day-20/src/part1.rs
@@ -1,143 +1,249 @@
 use std::collections::{HashMap, VecDeque};
 
-use itertools::Itertools;
-
 use crate::custom_error::AocError;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum Signal {
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum SignalType {
     High,
     Low,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum NodeType {
-    Broadcast,
-    FlipFlop(bool),
-    Conjunct,
-}
-
 #[derive(Debug)]
-struct Node {
-    name: String,
-    node_type: NodeType,
-    out_conns: Vec<String>,
-    in_conns: Vec<String>,
+struct Signal {
+    src: String,
+    dst: String,
+    signal_type: SignalType,
+}
+impl Signal {
+    fn forward(&self, conn: &str) -> Self {
+        Self {
+            src: self.dst.clone(),
+            dst: conn.to_string(),
+            signal_type: self.signal_type,
+        }
+    }
+
+    fn send(&self, conn: &str, signal_type: SignalType) -> Self {
+        Self {
+            src: self.dst.clone(),
+            dst: conn.to_string(),
+            signal_type,
+        }
+    }
 }
 
-impl Node {
-    fn from_str(s: &str) -> Self {
-        let mut parts = s.split(" -> ");
-        let name_with_type = parts.next().unwrap();
-        let conns = parts.next().unwrap();
-        let node_type = match name_with_type.chars().nth(0).unwrap() {
-            '&' => NodeType::Conjunct,
-            '%' => NodeType::FlipFlop(false), // Off by default
-            _ => NodeType::Broadcast,
-        };
-        let conns = conns.split(", ").map(|s| s.to_string()).collect();
-        let name = match node_type {
-            NodeType::Broadcast => name_with_type.to_string(),
-            _ => name_with_type[1..].to_string(),
-        };
+//-----------------------------------------------------------------------------
+trait Node {
+    fn name(&self) -> &str;
+    fn out_conns(&self) -> &[String];
+    fn set_incoming(&mut self, incoming: &Vec<String>);
+    fn process_signal(&mut self, signal: &Signal) -> Vec<Signal>;
+}
 
+//-----------------------------------------------------------------------------
+#[derive(Debug)]
+struct BroadcastNode {
+    name: String,
+    out_conns: Vec<String>,
+}
+impl BroadcastNode {
+    fn new(name: &str, conns: Vec<String>) -> Self {
         Self {
-            name,
-            node_type,
+            name: name.to_string(),
             out_conns: conns,
-            in_conns: vec![],
         }
     }
+}
 
-    fn process(&mut self, signal_type: Signal) -> Vec<(String, Signal)> {
-        match self.node_type {
-            NodeType::Broadcast => self.process_broadcast(signal_type),
-            NodeType::FlipFlop(on) => self.process_flipflop(on, signal_type),
-            NodeType::Conjunct => self.process_conjunct(signal_type),
-        }
+impl Node for BroadcastNode {
+    fn name(&self) -> &str {
+        &self.name
     }
 
-    fn process_broadcast(&self, signal_type: Signal) -> Vec<(String, Signal)> {
+    fn out_conns(&self) -> &[String] {
+        &self.out_conns
+    }
+
+    fn set_incoming(&mut self, _: &Vec<String>) {}
+
+    fn process_signal(&mut self, signal: &Signal) -> Vec<Signal> {
         self.out_conns
             .iter()
-            .map(|conn| (conn.clone(), signal_type))
+            .map(|conn| signal.forward(conn))
             .collect()
-    }
-
-    fn process_flipflop(&mut self, on: bool, signal_type: Signal) -> Vec<(String, Signal)> {
-        if signal_type == Signal::Low {
-            self.node_type = NodeType::FlipFlop(!on);
-            let new_pulse = if on { Signal::Low } else { Signal::High };
-            self.process_broadcast(new_pulse)
-        } else {
-            vec![]
-        }
-    }
-
-    fn process_conjunct(&self, signal_type: Signal) -> Vec<(String, Signal)> {
-        vec![]
     }
 }
 
+//-----------------------------------------------------------------------------
 #[derive(Debug)]
+struct FlipFlopNode {
+    name: String,
+    out_conns: Vec<String>,
+    on: bool,
+}
+
+impl FlipFlopNode {
+    fn new(name: &str, conns: Vec<String>) -> Self {
+        Self {
+            name: name.to_string(),
+            out_conns: conns,
+            on: false,
+        }
+    }
+}
+
+impl Node for FlipFlopNode {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn out_conns(&self) -> &[String] {
+        &self.out_conns
+    }
+
+    fn set_incoming(&mut self, _: &Vec<String>) {}
+
+    fn process_signal(&mut self, signal: &Signal) -> Vec<Signal> {
+        if signal.signal_type == SignalType::High {
+            return vec![];
+        }
+
+        self.on = !self.on;
+        let new_pulse = if self.on {
+            SignalType::Low
+        } else {
+            SignalType::High
+        };
+        self.out_conns
+            .iter()
+            .map(|conn| signal.send(conn, new_pulse))
+            .collect()
+    }
+}
+
+//-----------------------------------------------------------------------------
+#[derive(Debug)]
+struct ConjunctNode {
+    name: String,
+    out_conns: Vec<String>,
+    in_state: HashMap<String, bool>,
+}
+
+impl ConjunctNode {
+    fn new(name: &str, conns: Vec<String>) -> Self {
+        Self {
+            name: name.to_string(),
+            out_conns: conns,
+            in_state: HashMap::new(),
+        }
+    }
+}
+
+impl Node for ConjunctNode {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn out_conns(&self) -> &[String] {
+        &self.out_conns
+    }
+
+    fn set_incoming(&mut self, in_names: &Vec<String>) {
+        for name in in_names {
+            self.in_state.insert(name.to_string(), false);
+        }
+    }
+
+    fn process_signal(&mut self, signal: &Signal) -> Vec<Signal> {
+        let state_for_input = self.in_state.get_mut(&signal.src).unwrap();
+        *state_for_input = !*state_for_input;
+        let out_signal = if self.in_state.values().all(|v| *v) {
+            SignalType::Low
+        } else {
+            SignalType::High
+        };
+
+        self.out_conns
+            .iter()
+            .map(|conn| signal.send(conn, out_signal))
+            .collect()
+    }
+}
+
+type BoxedNode = Box<dyn Node>;
+
 struct Network {
-    nodes: HashMap<String, Node>,
-    high_pulses: u64,
-    low_pulses: u64,
+    nodes: HashMap<String, BoxedNode>,
+    pulse_counts: HashMap<SignalType, u64>,
 }
 
 impl Network {
     fn from_str(s: &str) -> Self {
-        let mut nodes: HashMap<String, Node> = HashMap::new();
+        let mut nodes: HashMap<String, BoxedNode> = HashMap::new();
         let mut in_conns: HashMap<String, Vec<String>> = HashMap::new();
         for line in s.lines() {
-            let node = Node::from_str(line.trim());
-            for conn in &node.out_conns {
+            let node = Self::node_from_str(line.trim());
+            for conn in node.out_conns() {
                 in_conns
                     .entry(conn.clone())
                     .or_default()
-                    .push(node.name.clone());
+                    .push(node.name().to_string());
             }
-            nodes.insert(node.name.clone(), node);
+            nodes.insert(node.name().to_string(), node);
         }
 
         for node in nodes.values_mut() {
-            node.in_conns = in_conns.remove(&node.name).unwrap_or_default();
+            let incoming_conns = in_conns.remove(node.name()).unwrap_or_default();
+            node.set_incoming(&incoming_conns);
         }
 
         Self {
             nodes,
-            high_pulses: 0,
-            low_pulses: 0,
+            pulse_counts: HashMap::new(),
+        }
+    }
+
+    fn node_from_str(s: &str) -> BoxedNode {
+        let mut parts = s.split(" -> ");
+        let name_with_type = parts.next().unwrap();
+        let node_type = name_with_type.chars().nth(0).unwrap();
+
+        let conns = parts.next().unwrap();
+        let conns = conns.split(", ").map(|s| s.to_string()).collect();
+
+        match node_type {
+            '&' => Box::new(ConjunctNode::new(&name_with_type[1..], conns)),
+            '%' => Box::new(FlipFlopNode::new(&name_with_type[1..], conns)),
+            _ => Box::new(BroadcastNode::new(&name_with_type, conns)),
         }
     }
 
     fn score(&self) -> u64 {
-        self.high_pulses * self.low_pulses
+        self.pulse_counts.values().product()
     }
 
     // Simulates a single pulse sent through the network, starting at the broadcaster module.
     fn press_button(&mut self) {
         let mut signal_queue = VecDeque::new();
-        signal_queue.push_back(("broadcaster".to_string(), Signal::Low));
+        signal_queue.push_back(Signal {
+            src: "button".to_string(),
+            dst: "broadcaster".to_string(),
+            signal_type: SignalType::Low,
+        });
 
-        while let Some((node_name, signal_type)) = signal_queue.pop_front() {
-            match signal_type {
-                Signal::High => self.high_pulses += 1,
-                Signal::Low => self.low_pulses += 1,
-            }
+        while let Some(signal) = signal_queue.pop_front() {
+            println!("Processing {:?}", signal);
+            self.pulse_counts
+                .entry(signal.signal_type)
+                .and_modify(|c| *c += 1)
+                .or_insert(1);
 
-            let node = self.nodes.get_mut(&node_name).unwrap();
-            println!(
-                "Processing node {} with signal {:?}: {:?}",
-                node_name, signal_type, node
-            );
-
-            let outgoing_signals = node.process(signal_type);
-            println!("Outgoing signals: {:?}", outgoing_signals);
-
-            for (conn, signal_type) in outgoing_signals {
-                signal_queue.push_back((conn, signal_type));
+            if let Some(dst_node) = self.nodes.get_mut(&signal.dst) {
+                let outgoing_signals = dst_node.process_signal(&signal);
+                println!("Outgoing signals: {:?}", outgoing_signals);
+                signal_queue.extend(outgoing_signals);
+            } else {
+                println!("Signal sent to an untyped node {}, dropping it", signal.dst);
             }
         }
     }
@@ -146,9 +252,10 @@ impl Network {
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
     let mut net = Network::from_str(input);
-    println!("{:#?}", net);
 
-    net.press_button();
+    for _ in 0..1000 {
+        net.press_button();
+    }
 
     Ok(net.score().to_string())
 }
@@ -167,4 +274,17 @@ mod tests {
         assert_eq!("32000000", process(input)?);
         Ok(())
     }
+
+    #[test]
+    fn test_process_harder() {
+        let input = "broadcaster -> a
+                     %a -> inv, con
+                     &inv -> b
+                     %b -> con
+                     &con -> output";
+        assert_eq!("11687500", process(input).unwrap());
+    }
 }
+
+// Submissions:
+// 697264874 - too low
