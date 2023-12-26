5ba8281b7bc830172b4e96e18e815d5d0658ac9a
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2023-12-26 17:09:17 -0500

    Day 22 part 1 (faster and cleaner)

--- day-22/src/part1.rs
+++ day-22/src/part1.rs
@@ -2,7 +2,7 @@ use std::cmp::{max, min};
 
 use crate::custom_error::AocError;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct World {
     bricks: Vec<Brick>,
 }
@@ -15,61 +15,54 @@ impl World {
 
         // Iterate over the bricks and move them down until they can't move anymore
         let mut max_z_seen = self.bricks.first().unwrap().end.z;
-        println!("Max z seen is {}", max_z_seen);
-
         for i in 0..self.bricks.len() {
-            println!("Checking brick {}", i);
-
-            // Move the brick down to at least the maximum z coordinate seen so far
+            // Move the brick down to at least the maximum z coordinate seen so far (where we can potentially hit another brick)
             let move_by = self.bricks[i].end.z - max_z_seen - 1;
             if move_by > 0 {
                 self.bricks[i].move_down(move_by)
             }
 
-            // Keep moving the brick down while possible
-            while self.can_move_down(&self.bricks[i]) {
+            // Keep moving the brick down while we can
+            while self.can_move_down(&self.bricks[i], None) {
                 self.bricks[i].move_down(1);
             }
-
-            println!(" - Brick {:?} has settled", self.bricks[i]);
-            if self.bricks[i].start.z > max_z_seen {
-                max_z_seen = self.bricks[i].start.z;
-                println!(" - Max z seen is now {}", max_z_seen);
-            }
+            max_z_seen = max(max_z_seen, self.bricks[i].start.z);
         }
-
-        // Sort the bricks by their z coordinate (lowest to highest)
-        self.bricks.sort_by(|a, b| a.end.z.cmp(&b.end.z));
     }
 
-    fn has_settled(&self) -> bool {
+    fn has_settled(&self, ignore_brick: Option<u64>) -> bool {
         for brick in &self.bricks {
-            if self.can_move_down(brick) {
-                println!("- Brick {:?} can move down", brick);
+            if let Some(ignore_brick) = ignore_brick {
+                if brick.id == ignore_brick {
+                    continue;
+                }
+            }
+
+            if self.can_move_down(brick, ignore_brick) {
                 return false;
             }
         }
         true
     }
 
-    fn can_move_down(&self, brick: &Brick) -> bool {
+    fn can_move_down(&self, brick: &Brick, ignore_neighbor: Option<u64>) -> bool {
         // Check if the brick is at the bottom of the world
         if brick.end.z == 1 {
             return false;
         }
 
         // Nearby bricks are those that have their z coordinate within 1 of the brick's z coordinates
-        for other_brick in &self.bricks {
-            if brick == other_brick {
-                continue;
-            }
+        let nearby = self
+            .bricks
+            .iter()
+            .filter(|b| b.end.z <= brick.start.z && b.start.z >= brick.end.z - 1)
+            .filter(|b| b.id != brick.id);
 
-            if other_brick.end.z > brick.start.z {
-                continue;
-            }
-
-            if other_brick.start.z < brick.end.z - 1 {
-                continue;
+        for other_brick in nearby {
+            if let Some(ignore_neighbor) = ignore_neighbor {
+                if other_brick.id == ignore_neighbor {
+                    continue;
+                }
             }
 
             for p1 in brick.points() {
@@ -85,7 +78,7 @@ impl World {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq)]
 struct Point3D {
     x: i32,
     y: i32,
@@ -110,30 +103,26 @@ impl Point3D {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq)]
 struct Brick {
-    name: char,
+    id: u64,
     start: Point3D,
     end: Point3D,
 }
 
 impl Brick {
-    fn new(name: char, start: Point3D, end: Point3D) -> Self {
+    fn new(id: u64, start: Point3D, end: Point3D) -> Self {
         // Make sure the points are ordered by their z coordinate (start should be higher)
         let (start, end) = if start.z > end.z {
             (start, end)
         } else {
             (end, start)
         };
-        Self { name, start, end }
+        Self { id, start, end }
     }
 
     fn move_down(&mut self, move_by: i32) {
-        if move_by == 0 {
-            return;
-        }
-
-        println!(" - Moving brick {:?} down by {}", self, move_by);
+        assert!(move_by > 0);
         self.start.z -= move_by;
         self.end.z -= move_by;
     }
@@ -166,31 +155,23 @@ impl Brick {
 pub fn process(input: &str) -> miette::Result<String, AocError> {
     let mut world = World { bricks: Vec::new() };
 
-    let mut name = 'A';
+    let mut id = 0;
     for line in input.lines() {
         let mut parts = line.trim().split('~');
         let left_point = Point3D::from_str(parts.next().unwrap());
         let right_point = Point3D::from_str(parts.next().unwrap());
-        world.bricks.push(Brick::new(name, left_point, right_point));
-        name = (name as u8 + 1) as char;
+        world.bricks.push(Brick::new(id, left_point, right_point));
+        id += 1;
     }
 
     // Let all the bricks settle down
     world.settle();
-    println!("Initial world has settled");
-
-    // Make sure settling is done
-    if !world.has_settled() {
-        panic!("Bricks are still moving");
-    }
+    assert!(world.has_settled(None));
 
     // Count all bricks that, if removed, would not cause any other brick to fall
     let mut count = 0;
     for brick in &world.bricks {
-        println!("Checking brick {:?}", brick);
-        let mut world_without_brick = world.clone();
-        world_without_brick.bricks.retain(|b| b != brick);
-        if world_without_brick.has_settled() {
+        if world.has_settled(Some(brick.id)) {
             count += 1;
         }
     }
@@ -216,19 +197,19 @@ mod tests {
     #[test]
     fn test_brick_points() {
         // A line like 2,2,2~2,2,2 means that both ends of the brick are at the same coordinate - in other words, that the brick is a single cube.
-        let brick = Brick::new('A', Point3D::new(0, 0, 0), Point3D::new(0, 0, 0));
+        let brick = Brick::new(0, Point3D::new(0, 0, 0), Point3D::new(0, 0, 0));
         assert_eq!(vec![Point3D::new(0, 0, 0)], brick.points());
 
         // Lines like 0,0,10~1,0,10 or 0,0,10~0,1,10 both represent bricks that are two cubes in volume, both oriented horizontally.
         // The first brick extends in the x direction, while the second brick extends in the y direction.
-        let brick1 = Brick::new('A', Point3D::new(0, 0, 10), Point3D::new(1, 0, 10));
-        let brick2 = Brick::new('A', Point3D::new(0, 0, 10), Point3D::new(0, 1, 10));
+        let brick1 = Brick::new(0, Point3D::new(0, 0, 10), Point3D::new(1, 0, 10));
+        let brick2 = Brick::new(0, Point3D::new(0, 0, 10), Point3D::new(0, 1, 10));
         assert_eq!(2, brick1.points().len());
         assert_eq!(2, brick2.points().len());
 
         // A line like 0,0,1~0,0,10 represents a ten-cube brick which is oriented vertically. One end of the brick is the cube located at 0,0,1,
         // while the other end of the brick is located directly above it at 0,0,10.
-        let brick = Brick::new('A', Point3D::new(0, 0, 1), Point3D::new(0, 0, 10));
+        let brick = Brick::new(0, Point3D::new(0, 0, 1), Point3D::new(0, 0, 10));
         assert_eq!(10, brick.points().len());
     }
 
