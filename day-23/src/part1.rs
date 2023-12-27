b1de51ad39fbab1a8e43cc2cf90ca8f7ac0c27de
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2023-12-27 09:21:28 -0500

    Day 23 part 2 WIP

--- day-23/src/part1.rs
+++ day-23/src/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     custom_error::AocError,
@@ -12,13 +12,27 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
     let start = Point::new(1, 0);
     let steps = 0;
     let visited = HashSet::new();
-    let longest = hike(&map, start, steps, visited);
+    let mut best_steps: HashMap<Point<i64>, u64> = HashMap::new();
+    let longest = hike(&map, start, steps, visited, &mut best_steps);
 
     Ok(longest.to_string())
 }
 
-fn hike(map: &CharMap, pos: Point<i64>, steps: u64, visited: HashSet<Point<i64>>) -> u64 {
+fn hike(
+    map: &CharMap,
+    pos: Point<i64>,
+    steps: u64,
+    visited: HashSet<Point<i64>>,
+    best: &mut HashMap<Point<i64>, u64>, // Best known steps to get to a point (longest)
+) -> u64 {
     let finish = Point::new(map.width() as i64 - 2, map.height() as i64 - 1);
+    let best_steps = best.get(&pos).copied().unwrap_or(0);
+    if steps < best_steps {
+        println!("Skipping {} because {} < {}", pos, steps, best_steps);
+        return 0;
+    }
+    best.insert(pos, steps);
+
     if pos == finish {
         println!("Found finish at {} in {} steps", pos, steps);
         return steps;
@@ -46,7 +60,7 @@ fn hike(map: &CharMap, pos: Point<i64>, steps: u64, visited: HashSet<Point<i64>>
             }
         }
 
-        let steps = hike(map, next, steps + 1, visited.clone());
+        let steps = hike(map, next, steps + 1, visited.clone(), best);
         candidates.push(steps);
     }
 
--- day-23/src/part2.rs
+++ day-23/src/part2.rs
@@ -1,8 +1,120 @@
-use crate::custom_error::AocError;
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    custom_error::AocError,
+    utils::{CharMap, Direction, Point},
+};
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let input = input
+        .replace("<", ".")
+        .replace(">", ".")
+        .replace("^", ".")
+        .replace("v", ".");
+
+    let map = CharMap::from_str_with_trim(&input, '#');
+
+    let start = Point::new(1, 0);
+    let visited = HashSet::new();
+
+    let mut best_steps = BestVisits::new();
+    best_steps.insert((start, Direction::South), 0);
+
+    let longest = hike(&map, start, Direction::South, visited, &mut best_steps);
+
+    Ok(longest.to_string())
+}
+
+type Visit = (Point<i64>, Direction);
+type BestVisits = HashMap<Visit, u64>;
+
+fn hike(
+    map: &CharMap,
+    pos: Point<i64>,
+    dir: Direction,
+    visited: HashSet<Point<i64>>,
+    best: &mut BestVisits, // Best known steps to get to a point (longest)
+) -> u64 {
+    let finish = Point::new(map.width() as i64 - 2, map.height() as i64 - 1);
+
+    let visit = (pos, dir);
+    let best_steps = best.get(&visit).copied().unwrap_or(0);
+    let steps = visited.iter().count() as u64;
+
+    if steps < best_steps {
+        //     println!(
+        //         "Skipping visit {:?} after {} steps because previously reached it in {}",
+        //         visit, steps, best_steps
+        //     );
+        //     return 0;
+    } else {
+        best.insert(visit, steps);
+    }
+
+    // println!("Found a way to visit {:?} in {} steps", visit, steps);
+    let mut visited = visited.clone();
+    visited.insert(pos);
+
+    if pos == finish {
+        println!(
+            "Found finish at {} in {} steps. Best = {}",
+            pos,
+            steps,
+            best.get(&visit).unwrap()
+        );
+        // print_visits(map, &pos, &visited, best);
+        return steps;
+    }
+
+    // Take next steps
+    let mut candidates = vec![];
+    for dir in Direction::each() {
+        let next = pos + dir.delta();
+        if visited.contains(&next) {
+            continue;
+        }
+
+        if *map.cell_for_point(&next) == '#' {
+            continue;
+        }
+
+        let res = hike(map, next, dir, visited.clone(), best);
+        candidates.push(res);
+    }
+
+    *candidates.iter().max().unwrap_or(&0)
+}
+
+fn print_visits(
+    map: &CharMap,
+    pos: &Point<i64>,
+    visited: &HashSet<Point<i64>>,
+    best: &mut HashMap<(Point<i64>, Direction), u64>,
+) {
+    println!("---------------------------------------------------------------------------------------------------------------------------");
+    // println!("Best visits: {:#?}", best);
+
+    // let debug_west = (Point::new(6, 13), Direction::West);
+    // assert!(best.contains_key(&debug_west));
+
+    // let debug_east = (Point::new(6, 13), Direction::East);
+    // assert!(best.contains_key(&debug_east));
+
+    // println!("---------------------------------------------------------------------------------------------------------------------------");
+
+    // Create a map where all visited points are marked with a direction char from the best visits map
+    let mut visited_map = map.clone();
+    // for (pos, dir) in best.keys() {
+    //     visited_map.set_cell_for_point(&pos, dir.to_char());
+    // }
+
+    for pos in visited {
+        visited_map.set_cell_for_point(&pos, 'O');
+    }
+
+    visited_map.print_with_current(pos, 'X');
+    println!("---------------------------------------------------------------------------------------------------------------------------");
 }
 
 #[cfg(test)]
@@ -11,8 +123,37 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "#.#####################
+                     #.......#########...###
+                     #######.#########.#.###
+                     ###.....#.>.>.###.#.###
+                     ###v#####.#v#.###.#.###
+                     ###.>...#.#.#.....#...#
+                     ###v###.#.#.#########.#
+                     ###...#.#.#.......#...#
+                     #####.#.#.#######.#.###
+                     #.....#.#.#.......#...#
+                     #.#####.#.#.#########v#
+                     #.#...#...#...###...>.#
+                     #.#.#v#######v###.###v#
+                     #...#.>.#...>.>.#.###.#
+                     #####v#.#.###v#.#.###.#
+                     #.....#...#...#.#.#...#
+                     #.#########.###.#.#.###
+                     #...###...#...#...#.###
+                     ###.###.#.###v#####v###
+                     #...#...#.#.>.>.#.>.###
+                     #.###.###.#.###.#.#v###
+                     #.....###...###...#...#
+                     #####################.#";
+        assert_eq!("154", process(input)?);
         Ok(())
     }
 }
+
+// Submissions:
+// 5130 - ?
+// 5466 - too low
+// 6055 - too low
+// 6079 - too low
+// 6122 - incorrect
